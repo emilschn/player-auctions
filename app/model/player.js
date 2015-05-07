cc1819edbe333aec5e98e7f609b9b33dc1ad46e6
Emilien Schneider <emilien.schneider@gmail.com>
2015-05-07 10:57:59 +0200

    Fin inclusion en bdd admin Signed-off-by: Emilien Schneider <emilien.schneider@gmail.com>

--- app/model/player.js
+++ app/model/player.js
@@ -8,4 +8,17 @@ var playerSchema = mongoose.Schema({
 	position : mongoose.Schema.ObjectId,
 	club : mongoose.Schema.ObjectId
 });
+playerSchema.methods.add = function(firstname, lastname, country, position, club) {
+	var buffer = 0;
+	if (firstname !== "" && lastname !== "" && country !== "" && position !== "" && club !== "") {
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.country = country;
+		this.position = position;
+		this.club = club;
+		this.save();
+		buffer = 1;
+	}
+	return buffer;
+};
 module.exports = mongoose.model('Player', playerSchema);
--- app/routes.js
+++ app/routes.js
@@ -40,16 +40,23 @@ module.exports = function(app, passport) {
 //****************************************
 //BACK-OFFICE
 	app.get('/admin/ajouter', isLoggedIn, function(req, res) {
+		//Récupération asynchrone de la liste des pays, des divisions, des clubs, des positions et des joueurs
 		var Country = require('./model/country');
 		Country.find(function(err, countryList) {
-			var Division = require('./model/division');
-			Division.find(function(err, divisionList) {
-				var Club = require('./model/club');
-				Club.find(function(err, clubList) {
-					res.render('admin/add.ejs', {user: req.user, countryList: countryList, divisionList: divisionList, clubList: clubList});
-				});
-			});
-		});
+		    
+		var Division = require('./model/division');
+		Division.find(function(err, divisionList) {
+		    
+		var Club = require('./model/club');
+		Club.find(function(err, clubList) {
+		    
+		var Position = require('./model/position');
+		Position.find(function(err, positionList) {
+		    
+		var Player = require('./model/player');
+		Player.find(function(err, playerList) {
+			res.render('admin/add.ejs', {user: req.user, countryList: countryList, divisionList: divisionList, clubList: clubList, positionList: positionList, playerList: playerList});
+		}); }); }); }); });
 	});
 	app.post('/admin/ajouter/pays', function(req, res) {
 		var Country = require('./model/country');
@@ -75,6 +82,26 @@ module.exports = function(app, passport) {
 		var Club = require('./model/club');
 		var newClub = new Club();
 		if (newClub.add(req.param('name'), req.param('division')) !== 0) {
+			res.redirect('/admin/ajouter');
+		} else {
+			res.setHeader('Content-Type', 'text/plain');
+			res.send(404, 'Il y a au moins un champ vide !');
+		}
+	});
+	app.post('/admin/ajouter/position', function(req, res) {
+		var Position = require('./model/position');
+		var newPosition = new Position();
+		if (newPosition.add(req.param('name')) !== 0) {
+			res.redirect('/admin/ajouter');
+		} else {
+			res.setHeader('Content-Type', 'text/plain');
+			res.send(404, 'Il y a au moins un champ vide !');
+		}
+	});
+	app.post('/admin/ajouter/joueur', function(req, res) {
+		var Player = require('./model/player');
+		var newPlayer = new Player();
+		if (newPlayer.add(req.param('firstname'), req.param('lastname'), req.param('country'), req.param('position'), req.param('club')) !== 0) {
 			res.redirect('/admin/ajouter');
 		} else {
 			res.setHeader('Content-Type', 'text/plain');
